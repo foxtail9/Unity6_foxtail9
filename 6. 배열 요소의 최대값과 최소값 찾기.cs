318f60e925a76daba45d004f92626fae3f83c42a
popuri <rayrose5175@gmail.com>
2024-08-27 16:34:58 +0900

    new file:

---
+++ 6. 배열 요소의 최대값과 최소값 찾기.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(){
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        int max = numbers[0];
+        int min = numbers[0];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+
+            if (numbers[i] < min)
+            {
+                min = numbers[i];
+            }
+        }
+
+        Console.WriteLine($"최대값: {max}");
+        Console.WriteLine($"최소값: {min}");
+    }
+
+}
