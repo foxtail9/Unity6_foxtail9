984542c1d65637d0c0d40b9acd3cf7b9d77c6d4f
popuri <rayrose5175@gmail.com>
2024-08-27 14:55:49 +0900

    new file:   "3. 팩토리얼 계산.cs"

---
+++ 3. 팩토리얼 계산.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num;
+
+        while (true)
+        {
+            Console.Write("숫자 입력 :  ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out num))
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("숫자가 아닙니다.");
+            }
+        }
+
+        int facto = Facfn(num);
+        Console.WriteLine($"{num}! = {facto}");
+    }
+    static int Facfn(int i)
+    {
+        if (i <= 1)
+            return 1;
+        else
+            return i * Facfn(i - 1);
+    }
+}
