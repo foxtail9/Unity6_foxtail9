cede28703cbc2ccda9e70f8f196e226f93125f7a
popuri <rayrose5175@gmail.com>
2024-08-27 15:52:37 +0900

    new file:

---
+++ 5. 이중 반복문을 사용한 구구단 출력.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //세로 출력
+        for (int i = 2; i <= 9; i++)
+        {
+            for (int k = 1; k <= 9; k++)
+            {
+                Console.WriteLine($"{i} x {k} = {i * k}");
+            }
+            Console.WriteLine("--------------------");
+        }
+
+        //가로 출력
+        for (int i = 2; i <= 9; i++)
+        {
+            for (int k = 1; k <= 9; k++)
+            {
+                Console.Write($"{i} x {k} = {i * k,2} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
